2a5f15ef745ff11577971aa67fbe00f073c78529
sipon <fakhrulislamsipon@gmail.com>
2025-09-19 01:16:17 +0600

    update navbar

--- src/page/home/Navbar.tsx
+++ src/page/home/Navbar.tsx
@@ -11,7 +11,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import { Link } from "react-router";
+import { Link } from "react-router"
 import Logo from "../../components/Logo";
 
 const pages = [
