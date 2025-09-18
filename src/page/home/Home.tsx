c3bce5748e5eb02782583f3b004ec2585deadb49
rifat-hasan <rifat-hasan121@example.com>
2025-09-18 18:53:41 +0600

    (Remove) navbar form Home component

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,13 +1,11 @@
 import FAQ from "./FAQ";
 import Footer from "./Footer";
 import Hero from "./Hero";
-import Navbar from "./Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            <Navbar></Navbar>
             <Hero></Hero>
             <FAQ></FAQ>
             <Footer></Footer>
